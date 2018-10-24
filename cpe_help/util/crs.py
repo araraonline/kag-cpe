a3bde6587249083d4884e5137f09bb29eacb7d45
araraonline <andre0lra@gmail.com>
2018-10-24 13:19:51 -0300

    Create src_help.util.crs module
    Already with some projections!
    
    Add descriptions later

---
+++ cpe_help/util/crs.py
@@ -0,0 +1,16 @@
+"""
+Module for defining Coordinate Reference Systems (CRS)
+
+*Do* import this module. Typing '+init esri:102739' every time isvery
+prone to errors.
+"""
+
+epsg4326 = {
+    'init': 'epsg:4326',
+    'no_defs': True,
+}
+
+esri102739 = {
+    'init': 'esri:102739',
+    'no_defs': True,
+}

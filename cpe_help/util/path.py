0237ce20f47a238288a8aaee67af0eb66da4ccef
araraonline <andre0lra@gmail.com>
2018-10-25 22:05:07 -0300

    Creat maybe_remove_*

--- cpe_help/util/path.py
+++ cpe_help/util/path.py
@@ -9,6 +9,34 @@ DATA_DIR = BASE_DIR / 'data'
 
 # Utils
 
+def maybe_rmfile(path):
+    """
+    Remove a file, if it exsits
+
+    Parameters
+    ----------
+    path : str or Path
+    """
+    import os
+    try:
+        os.remove(path)
+    except FileNotFoundError:
+        pass
+
+def maybe_rmtree(path):
+    """
+    Remove a directory tree, if it exists
+
+    Parameters
+    ----------
+    path : str or Path
+    """
+    from shutil import rmtree
+    try:
+        rmtree(path)
+    except FileNotFoundError:
+        pass
+
 def ensure_path(path):
     """
     Ensure that the directories that lead to a path exist

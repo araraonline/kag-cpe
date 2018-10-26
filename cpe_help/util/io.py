acd394697bace8dfb15f863b7b9bc40c05d056ef
araraonline <andre0lra@gmail.com>
2018-10-26 12:13:02 -0300

    Ensure path on io.save_json

--- cpe_help/util/io.py
+++ cpe_help/util/io.py
@@ -4,8 +4,11 @@ Module for input/output utils
 
 import json
 
+from cpe_help.util.path import ensure_path
+
 
 def save_json(obj, filename):
+    ensure_path(filename)
     with open(filename, mode='w') as f:
         json.dump(obj, f)
 

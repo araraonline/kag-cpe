eaa5941cd96e3083278aa3a17a42d8f9d65002a5
araraonline <andre0lra@gmail.com>
2018-10-26 13:40:03 -0300

    IO zipshp
    Lots of trouble here... We want to use the root of a zipfile to store
    the (shapefile) files. However if we try loading the root as
    gpd.read_file('/', vfs='zip://shapes.zip'), fiona will raise an
    Exception. I believe this is a bug, but also may be a usage
    misconception.
    
    To fix this, we can always store the files in a different VFS dir, e.g.
    gpd.read_file('/shapefile', vfs='zip://shapes.zip'). While it works,
    this won't work well for external data retrieved from websites, as the
    shapefiles are usually (up until now) found in the root dir.
    
    So, the solution I found (looks undocumented) was to use the zip URI
    directly. I don't know if this was intended behavior, but it works, and
    we are gonna roll with it. For example,
    gpd.read_file('zip://shapes.zip') just work!

--- cpe_help/util/io.py
+++ cpe_help/util/io.py
@@ -4,6 +4,8 @@ Module for input/output utils
 
 import json
 
+import geopandas as gpd
+
 from cpe_help.util.path import ensure_path
 
 
@@ -16,3 +18,27 @@ def save_json(obj, filename):
 def load_json(filename):
     with open(filename, mode='r') as f:
         return json.load(f)
+
+
+def load_zipshp(path):
+    """
+    Load a zipped shapefile
+
+    This is just the usual shapefile, but, instead of the usual
+    directory, we keep the contents in a zipfile, for the ease of
+    handling.
+    """
+    # https://commons.apache.org/proper/commons-vfs/filesystems.html
+    # using the URI directly doesn't seem documented in fiona
+    uri = f'zip://{path}'
+    return gpd.read_file(uri)
+
+
+def save_zipshp(df, path):
+    """
+    Save a zipped shapefile
+    """
+    ensure_path(path)
+
+    vfs = f'zip://{path}'
+    df.to_file('/', vfs=vfs)

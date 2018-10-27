bd0c9be20ff2956f4b0d080ccda37dd6fda7cde0
araraonline <andre0lra@gmail.com>
2018-10-26 21:19:30 -0300

    style

--- cpe_help/util/compression.py
+++ cpe_help/util/compression.py
@@ -8,18 +8,18 @@ from pathlib import Path
 from cpe_help.util.path import ensure_path, maybe_rmfile, maybe_rmtree
 
 
-def make_zipfile(path, root_dir):
+def make_zipfile(filename, root_dir):
     """
     Create a zip archive with the contents of root_dir
 
-    If path already exists, it will first be removed.
+    If filename is points to an existing file, it will first be removed.
 
     Parameters
     ----------
-    path : str or Path
-        The output ZIP archive.
+    filename : str or Path
+        The name of the file to create (e.g. 'foo.zip').
     root_dir : str or Path
-        The directory to copy contens from.
+        The directory to retrieve contents from.
 
     Returns
     -------
@@ -27,7 +27,7 @@ def make_zipfile(path, root_dir):
     """
     from shutil import make_archive
 
-    path = Path(path)
+    path = Path(filename)
     maybe_rmfile(path)
     ensure_path(path)
     make_archive(path.with_suffix(''), 'zip', root_dir)

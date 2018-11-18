c8440fa1a4c40274bff767db864f8c0a4190f684
araraonline <andre0lra@gmail.com>
2018-11-18 19:33:31 -0200

    preprocess

--- cpe_help/departments/dept4900033.py
+++ cpe_help/departments/dept4900033.py
@@ -2,9 +2,11 @@
 Module for department 49-00033
 """
 
+import geopandas
 import pandas
+import shapely
 
-from cpe_help import Department
+from cpe_help import Department, util
 
 
 class Department4900033(Department):
@@ -34,3 +36,43 @@ class Department4900033(Department):
 
     def save_preprocessed_arrests(self, df):
         df.to_pickle(self.preprocessed_arrests_path)
+
+    def preprocess_arrests(self):
+        """
+        Preprocess arrests table
+        """
+        df = self.load_external_arrests()
+
+        # drop time
+        df = df.drop('INCIDENT_TIME', axis=1)
+
+        # process date
+        df['INCIDENT_DATE'] = pandas.to_datetime(
+            df['INCIDENT_DATE'],
+            format='%m/%d/%y %H:%M',
+        )
+
+        # flag entries without lat/lon
+        df['LOCATION_GEOCODED'] = (
+            (df['LOCATION_LATITUDE'] != 0.0) |
+            (df['LOCATION_LONGITUDE'] != 0.0)
+        )
+
+        # infer CRS from shapefile
+        crs = self.load_external_shapefile().crs
+
+        # generate geometries from lat/lon
+        Point = shapely.geometry.Point
+        lat_lst = df['LOCATION_LATITUDE']
+        lon_lst = df['LOCATION_LONGITUDE']
+        coded_lst = df['LOCATION_GEOCODED']
+        geometry = [Point(lon, lat) if coded else Point()
+                    for lat, lon, coded in zip(lat_lst, lon_lst, coded_lst)]
+
+        # generate GeoDataFrame
+        df = geopandas.GeoDataFrame(df, geometry=geometry, crs=crs)
+
+        # project into default CRS
+        df = df.to_crs(util.crs.DEFAULT)
+
+        self.save_preprocessed_arrests(df)

09ec5b5554ae6656a01e48a5e9cadeb8b42e0759
araraonline <andre0lra@gmail.com>
2018-11-18 21:25:32 -0200

    Extract city boundaries

--- dodo.py
+++ dodo.py
@@ -290,3 +290,22 @@ def task_preprocess_4900033_arrests():
         'actions': [dept.preprocess_arrests],
         'clean': [dept.remove_preprocessed_arrests],
     }
+
+
+def task_extract_city_boundaries():
+    """
+    Extract city boundaries for each department
+    """
+    tiger = get_tiger()
+    for dept in list_departments():
+        yield {
+            'name': dept.name,
+            'file_dep': [
+                dept.guessed_city_path,
+                dept.guessed_state_path,
+            ],
+            'task_dep': ['download_place_boundaries'],
+            'targets': [dept.city_boundaries_path],
+            'actions': [dept.extract_city_boundaries],
+            'clean': [dept.remove_city_boundaries],
+        }

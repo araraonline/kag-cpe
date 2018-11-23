6a27d1babd01b6be65bc686ecc9b569326f60869
araraonline <andre0lra@gmail.com>
2018-11-23 13:50:40 -0200

    comment

--- output.py
+++ output.py
@@ -2,7 +2,7 @@
 dodo file for generating outputs
 
 This should not be run directly. Instead, all tasks here are imported at
-the end of the main dodo.py file and will be run from there.
+the main dodo.py file and will be run there.
 """
 
 from cpe_help import list_departments

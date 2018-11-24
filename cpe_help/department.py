d034a2d0fa8f42a8591b571f9db6ec02ebf1a5b2
araraonline <andre0lra@gmail.com>
2018-11-24 18:50:43 -0200

    Implement Department.__str__

--- cpe_help/department.py
+++ cpe_help/department.py
@@ -193,6 +193,9 @@ class Department():
         """
         self.name = name
 
+    def __str__(self):
+        return self.name
+
     def __repr__(self):
         return "{klass}({name!r})".format(
             klass=type(self).__name__,
